0403861d944205c8766c0317e3ee3f2de701f299
Vinícius Tessmann <vinicius@melhorenvio.com.br>
2019-07-10 10:17:14 -0300

    Adicionando novos componentes de pedidos

---
+++ assets/src/admin/components/Pedido/Destino.vue
@@ -0,0 +1,19 @@
+<template>
+    <span style="font-size: 14px;">
+        <strong>{{to.first_name}} {{to.last_name}}</strong> <br>
+        {{to.email}} <br>
+        {{to.phone}} <br>
+    </span>
+</template>
+<script>
+    export default {
+        props: {
+            to: {
+                type: Object,
+                default: () => ({}),
+            }
+        },
+        mounted () { 
+        }
+    }
+</script>
