079590299febe531f98e0171bbb79ea9552f90cf
Vinícius Schlee Tessmann <vinicius.tessmann@melhorenvio.com>
2020-07-16 14:37:14 -0300

    Ajustes versão e remoção temporario de azul cargo

--- melhor-envio-beta.php
+++ melhor-envio-beta.php
@@ -6,7 +6,7 @@ require __DIR__ . '/vendor/autoload.php';
 Plugin Name: Melhor Envio v2
 Plugin URI: https://melhorenvio.com.br
 Description: Plugin para cotação e compra de fretes utilizando a API da Melhor Envio.
-Version: 2.7.3
+Version: 2.7.6
 Author: Melhor Envio
 Author URI: melhorenvio.com.br
 License: GPL2
@@ -88,7 +88,7 @@ final class Base_Plugin {
      *
      * @var string
      */
-    public $version = '2.7.3';
+    public $version = '2.7.6';
 
     /**
      * Holds various class instances
@@ -357,7 +357,8 @@ final class Base_Plugin {
          */
         function sv_wc_my_orders_ship_to_column( $order ) {
             $data = (new TrackingService())->getTrackingOrder($order->id);
-            if( empty($data) || is_null($data['tracking'])) {
+
+            if(empty($data)) {
                 echo 'Aguardando postagem';
             } else {
                 echo '<a target="_blank" href="https://melhorrastreio.com.br/rastreio/'. $data .'">' . $data . '</a>';
