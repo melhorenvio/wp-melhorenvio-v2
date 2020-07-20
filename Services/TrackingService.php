e7bd09a843f7416596aa3b935133be5a45583bbd
Vinícius Schlee Tessmann <vinicius.tessmann@melhorenvio.com>
2020-07-20 20:41:12 -0300

    Separando a logica de inserir o rastreio do pedido do cliente final

--- Services/TrackingService.php
+++ Services/TrackingService.php
@@ -28,4 +28,45 @@ class TrackingService
     {
         return get_post_meta($order_id, self::TRACKING_MELHOR_ENVIO, true);
     }
+
+    /**
+     * Adds a new column to the "My Orders" table in the account.
+     *
+     * @param string[] $columns the columns in the orders table
+     * @return string[] updated columns
+     */
+    public function createTrackingColumnOrdersClient()
+    {
+        add_filter( 'woocommerce_my_account_my_orders_columns', function ($columns) {
+            $new_columns = array();
+            foreach ( $columns as $key => $name ) {
+                $new_columns[ $key ] = $name;
+                if ( 'order-status' === $key ) {
+                    $new_columns['tracking'] = __( 'Rastreio', 'textdomain' );
+                }
+            }
+            return $new_columns;
+        } );
+
+        $this->addTrackingToOrderClients();
+    }
+
+     /**
+     * Adds data to the custom "ship to" column in "My Account > Orders".
+     *
+     * @param \WC_Order $order the order object for the row
+     */
+    private function addTrackingToOrderClients()
+    {
+        add_action( 'woocommerce_my_account_my_orders_column_tracking', function($order){
+
+            $data = (new TrackingService())->getTrackingOrder($order->id);
+
+            if(empty($data)) {
+                echo 'Aguardando postagem';
+            } else {
+                echo '<a target="_blank" href="https://melhorrastreio.com.br/rastreio/'. $data .'">' . $data . '</a>';
+            }
+        } );
+    }
 }
