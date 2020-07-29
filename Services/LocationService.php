ea4b42cf71523666bd3a1be76fb00f4b6abd7670
Vinícius Schlee Tessmann <vinicius.tessmann@melhorenvio.com>
2020-07-29 20:23:36 -0300

    Separando logica de realizar curl

--- Services/LocationService.php
+++ Services/LocationService.php
@@ -27,8 +27,8 @@ class LocationService
     {
         $postalCode = str_replace("-", "", $postalCode);
         $postalCode = floatval($postalCode);
-     
-        if(empty($postalCode)) return null;
+
+        if (empty($postalCode)) return null;
 
         $address = $this->getAddressByPostalCodeLocationMelhorEnvio($postalCode);
 
@@ -36,7 +36,7 @@ class LocationService
             $address = $this->getAddressByPostalCodeLocationViaCep($postalCode);
         }
 
-        if(!$address) {
+        if ($address->erro) {
             return null;
         }
 
@@ -54,21 +54,15 @@ class LocationService
         $postalCode = str_pad($postalCode, 8, '0', STR_PAD_LEFT);
 
         $url = self::URL . $postalCode;
-        $curl = curl_init(); 
-        curl_setopt($curl, CURLOPT_URL, $url); 
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
-        $result = curl_exec($curl); 
-        curl_close($curl); 
 
-        $result = json_decode($result);
+        $result = $this->requestCurl($url);
 
-        if(isset($result->message)){
+        if (isset($result->message)) {
             return false;
         }
 
         return $result;
-    }   
+    }
 
     /**
      * Function to search for address in zip code api Via Cep
@@ -81,18 +75,31 @@ class LocationService
         $postalCode = str_pad($postalCode, 8, '0', STR_PAD_LEFT);
 
         $url = self::URL_VIA_CEP . $postalCode . '/json';
-        $curl = curl_init(); 
-        curl_setopt($curl, CURLOPT_URL, $url); 
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
-        $result = curl_exec($curl); 
-        $error  = curl_error($curl);
-        curl_close($curl); 
 
-        if(!empty($error)){
+        $result = $this->requestCurl($url);
+
+        if (!$result) {
             return false;
         }
 
+        return $result;
+    }
+
+    /**
+     * Function to execute a curl request
+     *
+     * @param string $url
+     * @return json
+     */
+    private function requestCurl($url)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        $result = curl_exec($curl);
+        curl_close($curl);
+
         return json_decode($result);
-    } 
+    }
 }
