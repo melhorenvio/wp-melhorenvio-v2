ef122290478bebbb35d24ec7c39740c95d963e24
Vinícius Schlee Tessmann <vinicius.tessmann@melhorenvio.com>
2020-07-31 07:44:01 -0300

    Ajuste na função de buscar dados da loja do vendedo

--- Services/SellerService.php
+++ Services/SellerService.php
@@ -3,6 +3,7 @@
 namespace Services;
 
 use Models\Address;
+use Models\Store;
 
 class SellerService
 {
@@ -13,40 +14,44 @@ class SellerService
      */
     public function getData()
     {
-        $data = $this->getDataApiMelhorEnvio();   
-
         $address = (new Address())->getAddressFrom();
 
-        if(isset($address['address']['id'])) {
-            $data->address->address = (isset($address['address']['address'])) ? $address['address']['address'] : null ;
-            $data->address->complement = (isset($address['address']['complement'])) ? $address['address']['complement'] : null ;
-            $data->address->number = (isset($address['address']['number'])) ? $address['address']['number'] : null ;
-            $data->address->district = (isset($address['address']['district'])) ? $address['address']['district'] : null ;
-            $data->address->city->city = (isset($address['address']['city'])) ? $address['address']['city'] : null ;
-            $data->address->city->state->state_abbr = (isset($address['address']['state'])) ? $address['address']['state'] : null ;
-            $data->address->postal_code = (isset($address['address']['postal_code'])) ? $address['address']['postal_code'] : null ;
+        $user = $this->getDataApiMelhorEnvio();
+
+        $store = $this->getStoreSelected();
+
+        if (isset($address['address']['id'])) {
+            $user->address->address = (isset($address['address']['address'])) ? $address['address']['address'] : null;
+            $user->address->complement = (isset($address['address']['complement'])) ? $address['address']['complement'] : null;
+            $user->address->number = (isset($address['address']['number'])) ? $address['address']['number'] : null;
+            $user->address->district = (isset($address['address']['district'])) ? $address['address']['district'] : null;
+            $user->address->city->city = (isset($address['address']['city'])) ? $address['address']['city'] : null;
+            $user->address->city->state->state_abbr = (isset($address['address']['state'])) ? $address['address']['state'] : null;
+            $user->address->postal_code = (isset($address['address']['postal_code'])) ? $address['address']['postal_code'] : null;
         }
 
         return (object) [
-            "name" => sprintf("%s %s", $data->firstname, $data->lastname),
-            "phone" => (isset($data->phone->phone)) ? $data->phone->phone : null,
-            "email" => $data->email,
-            "document" => $data->document,
-            "address" => $data->address->address,
-            "complement" => $data->address->complement,
-            "number" => $data->address->number,
-            "district" => $data->address->district,
-            "city" => $data->address->city->city,
-            "state_abbr" => $data->address->city->state->state_abbr,
+            "name" => ($store) ? $store->name :  sprintf("%s %s", $user->firstname, $user->lastname),
+            "phone" => (isset($user->phone->phone)) ? $user->phone->phone : null,
+            "email" => ($store) ? $store->email : $user->email,
+            "document" => $user->document,
+            "company_document" => ($store) ? $store->document : null,
+            "state_register" => ($store) ? $store->state_register : null,
+            "address" => $user->address->address,
+            "complement" => $user->address->complement,
+            "number" => $user->address->number,
+            "district" => $user->address->district,
+            "city" => $user->address->city->city,
+            "state_abbr" => $user->address->city->state->state_abbr,
             "country_id" => 'BR',
-            "postal_code" => $data->address->postal_code
-        ]; 
+            "postal_code" => $user->address->postal_code
+        ];
     }
 
     /**
      * Get data user on API Melhor Envio
      *
-     * @return array $data
+     * @return object $data
      */
     private function getDataApiMelhorEnvio()
     {
@@ -60,5 +65,31 @@ class SellerService
         }
 
         return $data;
+    }
+
+    /**
+     * Function to get store selected by seller.
+     *
+     * @return bool|object
+     */
+    public function getStoreSelected()
+    {
+        $stores = (new Store())->getStories();
+
+        if (!isset($stores['stores'])) {
+            return false;
+        }
+
+        $store = array_map(function ($store) {
+            if ($store['selected']) {
+                return $store;
+            }
+        }, $stores['stores']);
+
+        if (!isset($store[0]['document'])) {
+            return false;
+        }
+
+        return (object) $store[0];
     }
 }
