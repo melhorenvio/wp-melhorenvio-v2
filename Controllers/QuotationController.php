08cdfce11bdf5db3ff8fefb82885d0ce6aca4b96
Vinícius Schlee Tessmann <vinicius.tessmann@melhorenvio.com>
2020-08-11 17:40:03 -0300

    Ajustes cotação tela do produto

--- Controllers/QuotationController.php
+++ Controllers/QuotationController.php
@@ -175,8 +175,8 @@ class QuotationController
             $rates[] = [
                 'id' => $shippingMethod->id,
                 'name' => $shippingMethod->title,
-                'price' => (!empty($rate->meta_data['price'])) ? $rate->meta_data['price'] : null,
-                'delivery_time' => (!empty($rate->meta_data['delivery_time'])) ? $rate->meta_data['delivery_time'] : null,
+                'price' => $rate->meta_data['price'],
+                'delivery_time' => $rate->meta_data['delivery_time'],
             ];
         }
 
