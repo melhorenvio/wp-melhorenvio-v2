8b425a5c95758f470bcfa8ffd2554379bb836afa
Vinícius Schlee Tessmann <vinicius.tessmann@melhorenvio.com>
2020-09-03 15:11:19 -0300

    Ajustes JS

--- assets/js/shipping-product-page.js
+++ assets/js/shipping-product-page.js
@@ -1,66 +1,32 @@
 (function ($) {
     'use strict';
-
     $(function () {
-
         toggleCalculator();
-
         $(document).on('keyup', '.iptCep', function (e) {
-
             jQuery('.observation-shipping-free').hide();
-
             resetarTabela();
-
             if ($(this).val().length === 9) {
-
                 if ($(e.target).is('a#cfpp_credits')) { return; }
 
                 var url = $('#woocommerce-correios-calculo-de-frete-na-pagina-do-produto #calculo_frete_endpoint_url').val();
                 var cep = $('.iptCep').val();
-                var altura = $('#woocommerce-correios-calculo-de-frete-na-pagina-do-produto #calculo_frete_produto_altura').val();
-                var largura = $('#woocommerce-correios-calculo-de-frete-na-pagina-do-produto #calculo_frete_produto_largura').val();
-                var comprimento = $('#woocommerce-correios-calculo-de-frete-na-pagina-do-produto #calculo_frete_produto_comprimento').val();
-                var peso = $('#woocommerce-correios-calculo-de-frete-na-pagina-do-produto #calculo_frete_produto_peso').val();
-                var preco = $('#woocommerce-correios-calculo-de-frete-na-pagina-do-produto #calculo_frete_produto_preco').val();
                 var id_produto = $('#woocommerce-correios-calculo-de-frete-na-pagina-do-produto #id_produto').val();
 
                 var errors = [];
-
-                if (altura == 0) {
-                    errors.push('Produto com altura não informada');
-                }
-
-                if (largura == 0) {
-                    errors.push('Produto com largura não informada');
-                }
-
-                if (comprimento == 0) {
-                    errors.push('Produto com comprimento não informada');
-                }
-
-                if (peso == 0) {
-                    errors.push('Produto com peso não informado');
-                }
-
                 if (errors.length > 0) {
-
                     var row = '';
                     row = '<tr><td colspan="3">Ocorreu um erro ao obter informações sobre o valor do frete</td></tr>';
-
                     errors.map(item => {
                         row += `<tr><td colspan="3">${item}</td></tr>`;
                     });
-
                     $('#woocommerce-correios-calculo-de-frete-na-pagina-do-produto .resultado-frete table tbody').append(row);
                     esconderLoader();
                     exibirTabela();
-
                     return false;
                 }
 
                 let inpCEP = $(this);
                 inpCEP.attr('disabled', 'disabled');
-
                 exibirLoader();
                 esconderTabela();
                 resetarTabela();
@@ -94,20 +60,15 @@
                     success: function (response) {
                         var row = '';
                         let { data } = response;
-
                         data.map(item => {
-
                             if (item.observations && item.observations !== 'Frete Grátis') {
                                 jQuery('.observation-shipping-free').show();
                                 jQuery('.observation-shipping-free').html(item.observations);
                             }
-
                             let name = item.name
-
                             if (!item.delivery_time) {
                                 item.delivery_time = '';
                             }
-
                             row += `<tr><td>${name} ${item.delivery_time}: ${item.price}</td></tr>`;
                         });
 
@@ -152,17 +113,5 @@
             esconderTabela();
             $('#woocommerce-correios-calculo-de-frete-na-pagina-do-produto #calculo_frete_produto_preco').val(variation.display_price.toFixed(2));
         });
-
     });
 })(jQuery);
-
-function validateNumber(event) {
-    var key = window.event ? event.keyCode : event.which;
-    if (event.keyCode === 8 || event.keyCode === 46) {
-        return true;
-    } else if (key < 48 || key > 57) {
-        return false;
-    } else {
-        return true;
-    }
-};
