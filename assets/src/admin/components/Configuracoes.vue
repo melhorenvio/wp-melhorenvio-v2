fcb4e9f6c1b001fef66bfbe4055a852a7fa901eb
Vinícius Schlee Tessmann <vinicius.tessmann@melhorenvio.com>
2020-10-06 08:45:09 -0300

    Ajuste para desativar o loader após carregar as agências Jadlog pelo estado do usuário

--- assets/src/admin/components/Configuracoes.vue
+++ assets/src/admin/components/Configuracoes.vue
@@ -663,11 +663,6 @@ export default {
     showAgenciesState() {
         this.setLoader(true);
         this.agency = "";
-        let selectedAddress = this.addresses.filter(item => {
-          if (item.selected) {
-            return item;
-          }
-        });
         this.$http.post(`${ajaxurl}?action=get_agency_jadlog&my-state=true`)
             .then(response => {
                 this.setAgencies(response.data.agencies);
@@ -675,7 +670,7 @@ export default {
                 alert(error.response.data.message);
             })
             .finally(() => {
-                this.stopLoader();
+                this.setLoader(false);
             });
     }
   },
