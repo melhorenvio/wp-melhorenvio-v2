3bdf88d3c6bad2ad7a71eefba22cf62f1316ba10
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2020-12-17 08:19:26 -0300

    Ajuste chamada azul ecommerce

--- melhor-envio-beta.php
+++ melhor-envio-beta.php
@@ -281,7 +281,7 @@ final class Base_Plugin
             $methods['melhorenvio_via_brasil_rodoviario']  = 'WC_Melhor_Envio_Shipping_Via_Brasil_Rodoviario';
             $methods['melhorenvio_latam']  = 'WC_Melhor_Envio_Shipping_Latam';
             $methods['melhorenvio_azul_amanha']  = 'WC_Melhor_Envio_Shipping_Azul_Amanha';
-            $methods['melhorenvio_azull_ecommerce']  = 'WC_Melhor_Envio_Shipping_Azul_Ecommerce';
+            $methods['melhorenvio_azul_ecommerce']  = 'WC_Melhor_Envio_Shipping_Azul_Ecommerce';
             $methods['melhorenvio_correios_mini']  = 'WC_Melhor_Envio_Shipping_Correios_Mini';
             return $methods;
         });
