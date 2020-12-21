3e5eebb5cb3f1e5c0f3712bec8ffd87509edb7c8
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2020-12-21 07:29:15 -0300

    Adicionando validação para ver se existe class do WooCommerce

--- services_methods/class-wc-melhor-envio-shipping-latam.php
+++ services_methods/class-wc-melhor-envio-shipping-latam.php
@@ -1,26 +1,28 @@
 <?php
 
-class WC_Melhor_Envio_Shipping_Latam extends WC_Melhor_Envio_Shipping
-{
-    const ID = 'melhorenvio_latam';
-
-    const TITLE = 'Latam';
-
-    const METHOD_TITLE = "Latam (Melhor Envio)";
-
-    public $code = 10;
-
-    public $company = 'LATAM Cargo';
-
-    /**
-     * Initialize Latam.
-     *
-     * @param int $instance_id Shipping zone instance.
-     */
-    public function __construct($instance_id = 0)
+if (class_exists('WC_Melhor_Envio_Shipping')) {
+    class WC_Melhor_Envio_Shipping_Latam extends WC_Melhor_Envio_Shipping
     {
-        $this->id = self::ID;
-        $this->method_title = self::METHOD_TITLE;
-        parent::__construct($instance_id);
+        const ID = 'melhorenvio_latam';
+
+        const TITLE = 'Latam';
+
+        const METHOD_TITLE = "Latam (Melhor Envio)";
+
+        public $code = 10;
+
+        public $company = 'LATAM Cargo';
+
+        /**
+         * Initialize Latam.
+         *
+         * @param int $instance_id Shipping zone instance.
+         */
+        public function __construct($instance_id = 0)
+        {
+            $this->id = self::ID;
+            $this->method_title = self::METHOD_TITLE;
+            parent::__construct($instance_id);
+        }
     }
 }
