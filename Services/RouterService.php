9a2f7aa2aaef53ce9e1596d3eb6371c13c91a2df
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2021-03-10 10:54:13 -0300

    trocando isset por empty

--- Services/RouterService.php
+++ Services/RouterService.php
@@ -206,7 +206,7 @@ class RouterService
 
         foreach (['wp_ajax_get_address', 'wp_ajax_nopriv_get_address'] as $action) {
             add_action($action, function () use ($locationController) {
-                if (!isset($_GET['postal_code'])) {
+                if (empty($_GET['postal_code'])) {
                     return wp_send_json([
                         'error' => true,
                         'message' => 'Informar o campo "postal_code"'
@@ -239,7 +239,7 @@ class RouterService
         $payloadsController = new PayloadsController();
 
         add_action('wp_ajax_nopriv_get_payload', function () use ($payloadsController) {
-            if (!isset($_GET['post_id'])) {
+            if (empty($_GET['post_id'])) {
                 return wp_send_json([
                     'error' => true,
                     'message' => 'Informar o campo "post_id"'
@@ -249,7 +249,7 @@ class RouterService
         });
 
         add_action('wp_ajax_get_payload', function () use ($payloadsController) {
-            if (!isset($_GET['post_id'])) {
+            if (empty($_GET['post_id'])) {
                 return wp_send_json([
                     'error' => true,
                     'message' => 'Informar o campo "post_id"'
@@ -259,7 +259,7 @@ class RouterService
         });
 
         add_action('wp_ajax_destroy_payload', function () use ($payloadsController) {
-            if (!isset($_GET['post_id'])) {
+            if (empty($_GET['post_id'])) {
                 return wp_send_json([
                     'error' => true,
                     'message' => 'Informar o campo "post_id"'
@@ -269,14 +269,14 @@ class RouterService
         });
 
         add_action('wp_ajax_get_payload_cart', function() use ($payloadsController) {
-            if (!isset($_GET['post_id'])) {
+            if (empty($_GET['post_id'])) {
                 return wp_send_json([
                     'error' => true,
                     'message' => 'Informar o campo "post_id"'
                 ], 400);
             }
 
-            if (!isset($_GET['service'])) {
+            if (empty($_GET['service'])) {
                 return wp_send_json([
                     'error' => true,
                     'message' => 'Informar o campo "service"'
