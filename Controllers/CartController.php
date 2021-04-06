fb40554c6b131ddbe10f66a9794a26e3c8bcdf7a
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2021-04-06 13:31:49 -0300

    add dockblock

--- Controllers/CartController.php
+++ Controllers/CartController.php
@@ -6,6 +6,11 @@ use Services\CartService;
 
 class CartController 
 {
+    /**
+     * Controller function to get items in the shopping cart
+     * 
+     * @return array
+     */
     public function getInfoCart()
     {
         $data = (new CartService())->getInfoCart();
