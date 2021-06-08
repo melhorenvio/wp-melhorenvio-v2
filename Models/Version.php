4ca93ea3e8c663a338be7222b8297bb0ccca0136
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2021-06-08 09:41:15 -0300

    Ajustes no readme do projeto

--- Models/Version.php
+++ Models/Version.php
@@ -4,5 +4,5 @@ namespace Models;
 
 class Version
 {
-    const VERSION = '2.9.11';
+    const VERSION = '2.9.12';
 }
--- melhor-envio-beta.php
+++ melhor-envio-beta.php
@@ -8,16 +8,16 @@ require __DIR__ . '/vendor/autoload.php';
 Plugin Name: Melhor Envio v2
 Plugin URI: https://melhorenvio.com.br
 Description: Plugin para cotação e compra de fretes utilizando a API da Melhor Envio.
-Version: 2.9.11
+Version: 2.9.12
 Author: Melhor Envio
 Author URI: melhorenvio.com.br
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: baseplugin
-Tested up to: 5.0
+Tested up to: 2.9.9
 Requires PHP: 5.6
 WC requires at least: 4.0
-WC tested up to: 5.7
+WC tested up to: 5.7.2
 Domain Path: /languages
 */
 
