4fe557d5b9e95169d55225bb3a1a2a098c58a93d
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2022-05-17 15:42:54 -0300

    refactor: use map_deep

--- Helpers/SanitizeHelper.php
+++ Helpers/SanitizeHelper.php
@@ -6,22 +6,9 @@ class SanitizeHelper
 {
     public static function apply($data)
     {
-        $sanitizeData = [];
-
-        if (!is_array($data)) {
-            return sanitize_text_field($data);
-        }
-        
-        foreach ($data as $key => $item) {
-            if (is_array($item)) {
-                foreach ($item as $key2 => $field) {
-                    $sanitizeData[$key][$key2] = sanitize_text_field($field);
-                }
-                continue;
-            }
-            $sanitizeData[$key] = sanitize_text_field($item);
-        }
-
-        return $sanitizeData;
+        return map_deep(
+            wp_unslash($data),
+            'sanitize_text_field'
+        );
     }
 }
