b9c3300e75f0d951653858f54dac6406f852324a
eduardobattisti <eduardo.battisti@melhorenvio.com>
2022-05-18 13:48:31 -0300

    refactor: remove directive and update component props

--- assets/src/admin/components/ProductLink.vue
+++ assets/src/admin/components/ProductLink.vue
@@ -1,22 +1,29 @@
 <template>
-  <div v-safe-html="linkProduct" />
+  <div v-safe-html="customLink" />
 </template>
 <script>
-import Vue from "vue";
-import VueSafeHTML from "vue-safe-html";
-
-Vue.use(VueSafeHTML);
 
 export default {
   computed: {
-    linkProduct() {
-      const link = `/wp-admin/post.php?post=${this.id}&action=edit`;
+    customLink() {
+
+      const {id, definedLink, name} = this;
+
+      if(definedLink) {
+        return `<a target="_blank" rel="noreferrer noopener" href="${definedLink}">${name}</a>`;  
+      }
+
+      const link = `/wp-admin/post.php?post=${id}&action=edit`;
       return `<a target="_blank" rel="noreferrer noopener" href="${link}">${this.name}</a>`;
     },
   },
   props: {
     id: {
       type: Number,
+    },
+    definedLink: {
+      type: String,
+      default: null
     },
     name: {
       type: String,
