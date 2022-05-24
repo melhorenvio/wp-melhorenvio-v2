0f9ccb0e5037b870c12e0371ec747336a527dacb
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2022-05-24 16:45:27 -0300

    feat: run CS Fixer

--- Controllers/ShowCalculatorProductPage.php
+++ Controllers/ShowCalculatorProductPage.php
@@ -5,8 +5,8 @@ namespace MelhorEnvio\Controllers;
 /**
  * Controller to manage the product's paged calculator
  */
-class ShowCalculatorProductPage
-{
+class ShowCalculatorProductPage {
+
 
 	protected $product;
 	protected $height;
@@ -19,15 +19,14 @@ class ShowCalculatorProductPage
 	protected $baseUrl;
 	protected $whereShowCalculator;
 
-	public function __construct()
-	{
-		$this->basePath            = __DIR__;
-		$this->baseUrl             = plugin_dir_url(__FILE__);
-		$this->whereShowCalculator = (new ConfigurationController())->getWhereCalculatorValue();
+	public function __construct() {
+		 $this->basePath           = __DIR__;
+		$this->baseUrl             = plugin_dir_url( __FILE__ );
+		$this->whereShowCalculator = ( new ConfigurationController() )->getWhereCalculatorValue();
 
-		add_action('wp_enqueue_scripts', array($this, 'enqueueCssJsFrontend'));
-		add_action('wp_ajax_escutar_solicitacoes_de_frete', array($this, 'escutar_solicitacoes_de_frete'));
-		add_action('wp_ajax_nopriv_escutar_solicitacoes_de_frete', array($this, 'escutar_solicitacoes_de_frete'));
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueueCssJsFrontend' ) );
+		add_action( 'wp_ajax_escutar_solicitacoes_de_frete', array( $this, 'escutar_solicitacoes_de_frete' ) );
+		add_action( 'wp_ajax_nopriv_escutar_solicitacoes_de_frete', array( $this, 'escutar_solicitacoes_de_frete' ) );
 	}
 
 	/**
@@ -35,11 +34,10 @@ class ShowCalculatorProductPage
 	 *
 	 * @return void
 	 */
-	public function enqueueCssJsFrontend()
-	{
-		wp_enqueue_script('produto', BASEPLUGIN_ASSETS . '/js/shipping-product-page.js', array('jquery'));
-		wp_enqueue_script('produto-variacao', BASEPLUGIN_ASSETS . '/js/shipping-product-page-variacao.js', array('jquery'));
-		wp_enqueue_script('calculator', BASEPLUGIN_ASSETS . '/js/calculator.js', array('jquery'));
+	public function enqueueCssJsFrontend() {
+		wp_enqueue_script( 'produto', BASEPLUGIN_ASSETS . '/js/shipping-product-page.js', array( 'jquery' ) );
+		wp_enqueue_script( 'produto-variacao', BASEPLUGIN_ASSETS . '/js/shipping-product-page-variacao.js', array( 'jquery' ) );
+		wp_enqueue_script( 'calculator', BASEPLUGIN_ASSETS . '/js/calculator.js', array( 'jquery' ) );
 	}
 
 	/**
@@ -48,9 +46,8 @@ class ShowCalculatorProductPage
 	 *
 	 * @return void
 	 */
-	public function insertCalculator()
-	{
-		add_action($this->whereShowCalculator, array($this, 'isProductSingle'));
+	public function insertCalculator() {
+		add_action( $this->whereShowCalculator, array( $this, 'isProductSingle' ) );
 	}
 
 	/**
@@ -58,12 +55,11 @@ class ShowCalculatorProductPage
 	 *
 	 * @return void
 	 */
-	public function isProductSingle()
-	{
-		global $product;
-		if (is_product() && !$product->is_virtual('yes')) {
-			$this->prepareProduct($product);
-			add_action($this->whereShowCalculator, array($this, 'addCalculateShipping'), 11);
+	public function isProductSingle() {
+		 global $product;
+		if ( is_product() && ! $product->is_virtual( 'yes' ) ) {
+			$this->prepareProduct( $product );
+			add_action( $this->whereShowCalculator, array( $this, 'addCalculateShipping' ), 11 );
 		}
 	}
 
@@ -73,8 +69,7 @@ class ShowCalculatorProductPage
 	 * @param object $product
 	 * @return void
 	 */
-	public function prepareProduct($product)
-	{
+	public function prepareProduct( $product ) {
 		$this->product = $product;
 		$this->height  = $product->get_height();
 		$this->width   = $product->get_width();
@@ -87,13 +82,13 @@ class ShowCalculatorProductPage
 	/**
 	 * Adiciona o HTML do cálculo de frete na página do produto
 	 */
-	public function addCalculateShipping()
-	{
-		wp_enqueue_style('calculator-style', BASEPLUGIN_ASSETS . '/css/calculator.css');
-		wp_enqueue_script('calculator-script', BASEPLUGIN_ASSETS . '/js/calculator.js');
+	public function addCalculateShipping() {
+		wp_enqueue_style( 'calculator-style', BASEPLUGIN_ASSETS . '/css/calculator.css' );
+		wp_enqueue_script( 'calculator-script', BASEPLUGIN_ASSETS . '/js/calculator.js' );
 
-		echo esc_html(sprintf(
-			"<div id='woocommerce-correios-calculo-de-frete-na-pagina-do-produto' class='containerCalculator'>
+		echo esc_html(
+			sprintf(
+				"<div id='woocommerce-correios-calculo-de-frete-na-pagina-do-produto' class='containerCalculator'>
             <?php wp_nonce_field('solicita_calculo_frete', 'solicita_calculo_frete'); ?>
             <input type='hidden' id='calculo_frete_endpoint_url' value='%s'>
             <input type='hidden' id='calculo_frete_produto_altura' value='%f'>
@@ -131,14 +126,15 @@ class ShowCalculatorProductPage
                 </div>
             </div>
         </div>",
-			admin_url('admin-ajax.php'),
-			$this->height,
-			$this->width,
-			$this->length,
-			$this->weight,
-			$this->price,
-			$this->id,
-			'return usePostalCodeMask()'
-		));
+				admin_url( 'admin-ajax.php' ),
+				$this->height,
+				$this->width,
+				$this->length,
+				$this->weight,
+				$this->price,
+				$this->id,
+				'return usePostalCodeMask()'
+			)
+		);
 	}
 }
