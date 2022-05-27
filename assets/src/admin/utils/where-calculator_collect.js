ca0120e7cc419ac869506a61c9b2ca7da86d3368
eduardobattisti <eduardo.battisti@melhorenvio.com>
2022-05-27 10:43:14 -0300

    feat: create utils for where calculator collect

---
+++ assets/src/admin/utils/where-calculator_collect.js
@@ -0,0 +1,54 @@
+const where_calculator_collect = [
+  {
+    id: "none",
+    name: "Não exibir calculadora",
+  },
+  {
+    id: "woocommerce_before_single_product",
+    name: "Antes do titulo do produto (Depende do tema do projeto)",
+  },
+  {
+    id: "woocommerce_after_single_product",
+    name: "Depois do titulo do produto",
+  },
+  {
+    id: "woocommerce_single_product_summary",
+    name: "Antes da descrição do produto",
+  },
+  {
+    id: "woocommerce_before_add_to_cart_form",
+    name: "Antes do fórmulario de comprar",
+  },
+  {
+    id: "woocommerce_before_variations_form",
+    name: "Antes das opçoes do produto",
+  },
+  {
+    id: "woocommerce_before_add_to_cart_button",
+    name: "Antes do botão de comprar",
+  },
+  {
+    id: "woocommerce_before_single_variation",
+    name: "Antes do campo de variações",
+  },
+  {
+    id: "woocommerce_single_variation",
+    name: "Antes das variações",
+  },
+  {
+    id: "woocommerce_after_add_to_cart_form",
+    name: "Depois do botão de comprar",
+  },
+  {
+    id: "woocommerce_product_meta_start",
+    name: "Antes das informações do produto",
+  },
+  {
+    id: "woocommerce_share",
+    name: "Depois dos botões de compartilhamento",
+  },
+];
+
+export {
+  where_calculator_collect
+}
