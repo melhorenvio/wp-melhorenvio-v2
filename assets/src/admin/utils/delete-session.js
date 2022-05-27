5b7ecd89cca48cb7820bc31af667c3ab9ff28c5b
eduardobattisti <eduardo.battisti@melhorenvio.com>
2022-05-27 11:00:34 -0300

    feat: add default export

--- assets/src/admin/utils/delete-session.js
+++ assets/src/admin/utils/delete-session.js
@@ -1,3 +1,3 @@
-function deleteSession() {
+export default function deleteSession() {
   return `${ajaxurl}?action=delete_melhor_envio_session&_wpnonce=${wpApiSettings.nonce_configs}`;
 }
--- assets/src/admin/utils/verify-token.js
+++ assets/src/admin/utils/verify-token.js
@@ -1,3 +1,3 @@
-function verifyToken() {
+export default function verifyToken() {
   return `${ajaxurl}?action=verify_token&_wpnonce=${wpApiSettings.nonce_tokens}`;
 };
