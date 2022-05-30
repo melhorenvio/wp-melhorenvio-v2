97fc7439c95e4509b557b9a97ecfc423178dadac
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2022-05-30 14:13:38 -0300

    fix: adjust wp_nonce helper

--- Helpers/WpNonceValidatorHelper.php
+++ Helpers/WpNonceValidatorHelper.php
@@ -4,7 +4,7 @@ namespace MelhorEnvio\Helpers;
 
 class WpNonceValidatorHelper {
 
-	public function check( $wp_nonce, $type ) {
+	public static function check( $wp_nonce, $type ) {
 		if ( ! wp_verify_nonce( $wp_nonce, $type ) ) {
 			return wp_send_json( array(), 403 );
 		}
