9df7b74ff79c702de68abb10bceae8015d67e445
JoaoPedro0000 <joao.lopes@melhorenvio.com>
2024-04-04 13:39:42 -0300

    fix: validation if parentId is empty

--- Services/OrdersProductsService.php
+++ Services/OrdersProductsService.php
@@ -24,7 +24,7 @@ class OrdersProductsService {
 		}
 
 		foreach ($products as $key => $product) {
-			if (isset($product->parentId)){
+			if (!empty($product->parentId)){
 				unset($products[$key]);
 			}
 		}
