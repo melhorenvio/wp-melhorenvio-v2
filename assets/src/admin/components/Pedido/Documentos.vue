9f2a6ffb7ddb71e99c0b7c2258f60faf57788ab7
JulianaViccari <juliana.viccari@melhorenvio.com>
2026-03-25 09:41:00 -0300

    wip

--- assets/src/admin/components/Pedido/Documentos.vue
+++ assets/src/admin/components/Pedido/Documentos.vue
@@ -14,7 +14,9 @@
             item.quotation.choose_method == services.LATAM_JUNTOS ||
             item.quotation.choose_method == services.AZUL_AMANHA ||
             item.quotation.choose_method == services.AZUL_ECOMMERCE ||
-            item.quotation.choose_method == services.BUSLOG_RODOVIARIO
+            item.quotation.choose_method == services.BUSLOG_RODOVIARIO ||
+            item.quotation.choose_method == services.TOTAL_EXPRESS_STANDARD ||
+            item.quotation.choose_method == services.TOTAL_EXPRESS_E_TOTAL
           "
         >
           <fieldset class="checkLine">
