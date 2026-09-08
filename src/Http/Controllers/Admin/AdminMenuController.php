68dca591572850c147ccb8056a092fe5cc22bdfd
João Pedro Lopes <joao.lopes@melhorenvio.com>
2026-09-08 17:17:08 -0300

    Revert "fix(admin): fix iframe height to fill viewport on integrador page"
    This reverts commit a4adf5a9e81102f5f465c369c2201d473b987a2d.

--- src/Http/Controllers/Admin/AdminMenuController.php
+++ src/Http/Controllers/Admin/AdminMenuController.php
@@ -72,10 +72,6 @@ final class AdminMenuController {
 		remove_all_actions( 'all_admin_notices' );
 		?>
 		<style>
-		.melhor-envio-integrador-page {
-			overflow: hidden;
-		}
-
 		.melhor-envio-integrador-page #wpcontent, .melhor-envio-integrador-page #wpbody-content {
 			overflow-x: initial !important;
 		}
